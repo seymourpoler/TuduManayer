028c4337df6ad4403d68aba246a47095452c5269
seymourpoler <seymourpoler@gmail.com>
2020-07-31 11:44:26 +0200

    adding repository

---
+++ java/TuduManager/repository.postgres.sql2o/src/main/java/net/seymourpoler/tudumanager/repository/postgres/sql2o/todo/DeleteTodoRepository.java
@@ -0,0 +1,10 @@
+package net.seymourpoler.tudumanager.repository.postgres.sql2o.todo;
+
+import net.seymourpoler.tudumanager.domain.todo.delete.IDeleteTodoRepository;
+
+public class DeleteTodoRepository implements IDeleteTodoRepository {
+    @Override
+    public void delete(Integer todoId) {
+
+    }
+}
