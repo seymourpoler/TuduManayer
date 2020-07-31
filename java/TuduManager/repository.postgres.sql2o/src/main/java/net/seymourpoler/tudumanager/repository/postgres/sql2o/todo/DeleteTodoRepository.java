fe59ec52451f116b37e3e23d928f7f817685df83
seymourpoler <seymourpoler@gmail.com>
2020-07-31 12:15:57 +0200

    implementing DeleteTodoRepository

--- java/TuduManager/repository.postgres.sql2o/src/main/java/net/seymourpoler/tudumanager/repository/postgres/sql2o/todo/DeleteTodoRepository.java
+++ java/TuduManager/repository.postgres.sql2o/src/main/java/net/seymourpoler/tudumanager/repository/postgres/sql2o/todo/DeleteTodoRepository.java
@@ -1,10 +1,27 @@
 package net.seymourpoler.tudumanager.repository.postgres.sql2o.todo;
 
 import net.seymourpoler.tudumanager.domain.todo.delete.IDeleteTodoRepository;
+import net.seymourpoler.tudumanager.repository.postgres.sql2o.DataBaseConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DeleteTodoRepository implements IDeleteTodoRepository {
+    private final DataBaseConnectionFactory dataBaseConnectionFactory;
+
+    @Autowired
+    public DeleteTodoRepository(DataBaseConnectionFactory dataBaseConnectionFactory) {
+        this.dataBaseConnectionFactory = dataBaseConnectionFactory;
+    }
+
     @Override
     public void delete(Integer todoId) {
-
+        final String sql = "DELETE public.todos WHERE Id = :todoId";
+        try (var connection = dataBaseConnectionFactory.create()) {
+            connection
+                    .createQuery(sql)
+                    .addParameter("todoId", todoId)
+                    .executeUpdate();
+        }
     }
 }
