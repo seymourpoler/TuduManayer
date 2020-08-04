b79f555ca37fb22269d2bceb351b11fe77b6072a
seymourpoler <seymourpoler@gmail.com>
2020-08-04 11:34:59 +0200

    green: return_not_found_when_is_not_found

--- java/TuduManager/web.api.spring.boot/src/main/java/net/seymourpoler/tudumanager/web/api/spring/boot/todo/FindTodoController.java
+++ java/TuduManager/web.api.spring.boot/src/main/java/net/seymourpoler/tudumanager/web/api/spring/boot/todo/FindTodoController.java
@@ -2,6 +2,7 @@ package net.seymourpoler.tudumanager.web.api.spring.boot.todo;
 
 import net.seymourpoler.tudumanager.domain.todo.find.IFindTodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,11 @@ public class FindTodoController {
 
     @GetMapping("/api/todos/{todoId}")
     public ResponseEntity find(@PathVariable Integer todoId){
+        var todoMaybe = findTodoService.find(todoId);
+        if(!todoMaybe.isPresent()){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
         throw new RuntimeException();
+
     }
 }
