6354b9db0879cb25bd5728f99c31fc79a0baeb19
seymourpoler <seymourpoler@gmail.com>
2020-08-09 01:22:09 +0200

    refactor: remove dead code

--- java/TuduManager/domain/src/main/java/net/seymourpoler/tudumanager/domain/todo/create/models/Todo.java
+++ java/TuduManager/domain/src/main/java/net/seymourpoler/tudumanager/domain/todo/create/models/Todo.java
@@ -6,12 +6,10 @@ public class Todo {
     public String title;
     public String description;
     public LocalDateTime creationDate;
-    public LocalDateTime updatedDate;
 
     public Todo(String title, String description) {
         this.title = title;
         this.description = description;
         creationDate = LocalDateTime.now();
-        updatedDate = null;
     }
 }
