f2bb7a173650c7edc2eaa2ff700cfb0ed2d70d58
seymourpoler <seymourpoler@gmail.com>
2020-08-10 19:00:20 +0200

    implementing SearchTodoRepository

---
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/SearchTodoRepository.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+using TuduManayer.Domain.Todo.Search;
+
+namespace TuduManayer.Repository.Postgres.EntityFramework
+{
+    public class SearchTodoRepository : ISearchTodoRepository
+    {
+        private readonly DataBaseContextFactory dataBaseContextFactory;
+
+        public SearchTodoRepository(DataBaseContextFactory dataBaseContextFactory)
+        {
+            this.dataBaseContextFactory = dataBaseContextFactory;
+        }
+
+        public IReadOnlyCollection<Domain.Todo.Search.Models.Todo> Search(string searchText)
+        {
+            using var dbContext = dataBaseContextFactory.Create();
+            return dbContext.Todos
+                .ToList()
+                .Where(x => x.title.Contains(searchText) || x.description.Contains(searchText))
+                .Select(x => new Domain.Todo.Search.Models.Todo {Id = x.id, Title = x.title})
+                .ToList()
+                .AsReadOnly();
+        }
+    }
+}
