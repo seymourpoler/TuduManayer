91d5a7a30b3b7117b7a773c0e91876793b4a8cf0
seymourpoler <seymourpoler@gmail.com>
2020-11-01 07:48:51 +0100

    implemeting delete todo repository

---
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/DeleteTodoRepository.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using TuduManayer.Domain.Todo.Delete;
+
+namespace TuduManayer.Repository.Postgres.EntityFramework
+{
+    public class DeleteTodoRepository : IDeleteTodoRepository
+    {
+        private readonly DataBaseContextFactory dataBaseContextFactory;
+
+        public DeleteTodoRepository(DataBaseContextFactory dataBaseContextFactory)
+        {
+            this.dataBaseContextFactory = dataBaseContextFactory;
+        }
+
+        public void Delete(int todoId)
+        {
+            using var dbContext = dataBaseContextFactory.Create();
+            var todo = dbContext.Todos.Single(x => x.id == todoId);
+            dbContext.Todos.Remove(todo);
+            dbContext.SaveChanges();
+        }
+    }
+}
