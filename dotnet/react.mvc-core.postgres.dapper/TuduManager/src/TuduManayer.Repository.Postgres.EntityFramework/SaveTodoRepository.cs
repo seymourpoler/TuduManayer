9d6e897888e937701e026e08519ade9d5caf9cec
rodriguezl <rodriguezl@ryanair.com>
2020-11-17 17:55:28 +0100

    implementing Save in Save Todo Repository

---
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/SaveTodoRepository.cs
@@ -0,0 +1,26 @@
+﻿using TuduManayer.Domain.Todo.Create;
+
+namespace TuduManayer.Repository.Postgres.EntityFramework
+{
+    public class SaveTodoRepository : ISaveTodoRepository
+    {
+        private readonly DataBaseContextFactory contextFactory;
+
+        public SaveTodoRepository(DataBaseContextFactory contextFactory)
+        {
+            this.contextFactory = contextFactory;
+        }
+
+        public void Save(Domain.Todo.Create.Models.Todo todo)
+        {
+            using var dbContext = contextFactory.Create();
+            var entity = new TuduManayer.Repository.Postgres.EntityFramework.Todo
+            {
+                title = todo.Title,
+                description = todo.Description
+            };
+            dbContext.Todos.Add(entity);
+            dbContext.SaveChanges();
+        }
+    }
+}
