56322f85c6320e3bbbda0c04c8ca9e62fae241ad
rodriguezl <rodriguezl@ryanair.com>
2020-11-23 16:04:34 +0100

    implementing updatetodorepository

---
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/Todo/Update/UpdateTodoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using TuduManayer.Domain.Todo.Update;
+
+namespace TuduManayer.Repository.Postgres.EntityFramework.Todo.Update
+{
+    public class UpdateTodoRepository : IUpdateTodoRepository
+    {
+        private readonly DataBaseContextFactory dataBaseContextFactory;
+
+        public UpdateTodoRepository(DataBaseContextFactory dataBaseContextFactory)
+        {
+            this.dataBaseContextFactory = dataBaseContextFactory;
+        }
+
+        public void Update(Domain.Todo.Update.Models.Todo todo)
+        {
+            using var dbContext = dataBaseContextFactory.Create();
+            var entity = dbContext.Todos.First(x => x.id == todo.Id);
+            entity.title = todo.Title;
+            entity.description = todo.Description;
+            dbContext.SaveChanges();
+        }
+    }
+}
