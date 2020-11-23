ae793de9ca0bafa28440c2cede69ef938c0fc5a3
Luis Rodriguez Alejos <rodriguezl@ryanair.com>
2020-11-23 17:25:03 +0100

    adding entityframework decoration

--- dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/Todo/Models/Todo.cs
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/Todo/Models/Todo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TuduManayer.Repository.Postgres.EntityFramework.Todo.Models
@@ -6,11 +7,19 @@ namespace TuduManayer.Repository.Postgres.EntityFramework.Todo.Models
     [Table("todos")]
     public class Todo
     {
+        [Key]
         public int id { get; set; }
+        
+        [Required]
+        [StringLength(255)]
         public string title { get; set; }
+
+        [StringLength(255)]
         public string description { get; set; }
+        
+        [Required]
         public DateTime creation_date { get; set; }
+        
         public DateTime? updated_date { get; set; }
     }
-    
 }
