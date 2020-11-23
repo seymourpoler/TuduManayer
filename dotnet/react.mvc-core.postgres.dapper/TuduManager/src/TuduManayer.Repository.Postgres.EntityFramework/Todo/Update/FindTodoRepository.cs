371064ce48f149c36763da2dd5e89e3851969f4a
rodriguezl <rodriguezl@ryanair.com>
2020-11-23 15:49:29 +0100

    implementing FindTodoRepository

--- dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/Todo/Update/FindTodoRepository.cs
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/Todo/Update/FindTodoRepository.cs
@@ -1,12 +1,25 @@
-﻿using TuduManayer.Domain.Todo.Update;
+﻿using System.Linq;
+using TuduManayer.Domain.Todo.Update;
 
 namespace TuduManayer.Repository.Postgres.EntityFramework.Todo.Update
 {
     public class FindTodoRepository : IFindTodoRepository
     {
+        private readonly DataBaseContextFactory dataBaseContextFactory;
+
+        public FindTodoRepository(DataBaseContextFactory dataBaseContextFactory)
+        {
+            this.dataBaseContextFactory = dataBaseContextFactory;
+        }
+
         public Domain.Todo.Update.Models.Todo FindById(int todoId)
         {
-            throw new System.NotImplementedException();
+            using var dbContext = dataBaseContextFactory.Create();
+            var entity = dbContext.Todos.First(x => x.id == todoId);
+            return new Domain.Todo.Update.Models.Todo(
+                entity.id,
+                entity.title,
+                entity.description);
         }
     }
 }
