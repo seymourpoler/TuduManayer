e88e3ef6b1db70221a43c28bf36298f2e57f3df2
rodriguezl <rodriguezl@ryanair.com>
2020-12-11 15:23:33 +0100

    green return_not_found_when_is_not_found

--- dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Domain/Todo/FindById/FindByIdService.cs
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Domain/Todo/FindById/FindByIdService.cs
@@ -1,6 +1,5 @@
 namespace TuduManayer.Domain.Todo.FindById
 {
-    
     public interface IFindByTodoIdService
     {
         ServiceExecutionResultWithModel<Models.Todo> Find(int id);
@@ -8,8 +7,16 @@ namespace TuduManayer.Domain.Todo.FindById
     
     public class FindByTodoIdService : IFindByTodoIdService
     {
+        private readonly IExistTodoRepository existTodoRepository;
+
+        public FindByTodoIdService(IExistTodoRepository existTodoRepository)
+        {
+            this.existTodoRepository = existTodoRepository;
+        }
+
         public ServiceExecutionResultWithModel<Models.Todo> Find(int id)
         {
+            if(!existTodoRepository.Exist(id)) return ServiceExecutionResultWithModel<Models.Todo>.WitError();
             throw new System.NotImplementedException();
         }
     }
---
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/test/TuduManayer.Domain.Test/Todo/FindById/FindByIdServiceShould.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using Shouldly;
+using TuduManayer.Domain.Todo;
+using TuduManayer.Domain.Todo.FindById;
+using Xunit;
+
+namespace TuduManayer.Domain.Test.Todo.FindById
+{
+    public class FindByIdServiceShould
+    {
+        [Fact]
+        public void return_not_found_when_is_not_found()
+        {
+            const int someTodoId = 3;
+            Mock<IExistTodoRepository> existRepository = new Mock<IExistTodoRepository>();
+            existRepository
+                .Setup(x => x.Exist(someTodoId))
+                .Returns(false);
+            var service = new FindByTodoIdService(existRepository.Object);
+
+            var result = service.Find(someTodoId);
+            
+            result.IsOk.ShouldBeFalse();
+        }
+    }
+}
