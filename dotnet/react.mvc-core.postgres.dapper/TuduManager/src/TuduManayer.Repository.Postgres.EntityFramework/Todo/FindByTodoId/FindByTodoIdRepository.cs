6603d55c2cbd8e3d8ebe996eb7bf5ec2d2440024
rodriguezl <rodriguezl@ryanair.com>
2020-12-14 17:56:28 +0100

    implementing find by id

--- dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/Todo/FindByTodoId/FindByTodoIdRepository.cs
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Repository.Postgres.EntityFramework/Todo/FindByTodoId/FindByTodoIdRepository.cs
@@ -1,12 +1,22 @@
-﻿using TuduManayer.Domain.Todo.FindById;
+﻿using System.Linq;
+using TuduManayer.Domain.Todo.FindById;
 
 namespace TuduManayer.Repository.Postgres.EntityFramework.Todo.FindByTodoId
 {
     public class FindByTodoIdRepository : IFindByTodoIdRepository
     {
+        private readonly DataBaseContextFactory dataBaseContextFactory;
+
+        public FindByTodoIdRepository(DataBaseContextFactory dataBaseContextFactory)
+        {
+            this.dataBaseContextFactory = dataBaseContextFactory;
+        }
+
         public Domain.Todo.FindById.Models.Todo Find(int id)
         {
-            throw new System.NotImplementedException();
+            using var dbContext = dataBaseContextFactory.Create();
+            var entity = dbContext.Todos.Single(x => x.id == id);
+            return new Domain.Todo.FindById.Models.Todo(id, entity.title, entity.description);
         }
     }
 }
