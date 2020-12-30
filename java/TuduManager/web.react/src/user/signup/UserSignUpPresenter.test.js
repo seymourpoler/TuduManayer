8abbfe94eccf4036b1febc4e675df611e4a78daf
seymourpoler <seymourpoler@gmail.com>
2020-12-30 17:23:46 +0100

    red when signUp is requested, shows error if there is any erro

--- java/TuduManager/web.react/src/user/signup/UserSignUpPresenter.test.js
+++ java/TuduManager/web.react/src/user/signup/UserSignUpPresenter.test.js
@@ -5,7 +5,7 @@ import {createHttp} from "../../Http";
 import {UserSignUpService} from "./UserSignUpService";
 import {createUserSignUpView} from "./UserSignUpView";
 
-describe('User SignUp Pesenter', () =>{
+describe('User SignUp Presenter', () =>{
 
     let view, service, presenter, http;
 
@@ -28,6 +28,20 @@ describe('User SignUp Pesenter', () =>{
             await presenter.signUp(userSigningUpRequest);
 
             expect(view.showInternalServerError).toHaveBeenCalled();
+        });
+
+        it('shows error if there is any error', async () =>{
+            const userSigningUpRequest = {email: '', password: 'password'};
+            const errors = [{fieldId: 'Email', errorCode: 'Required'}];
+            http.post = () => {
+                return {
+                    statusCode: HttpStatusCode.badRequest,
+                    body: errors };
+            };
+
+            await presenter.signUp(userSigningUpRequest);
+
+            expect(view.showErrors).toHaveBeenCalledWith(errors);
         });
     });
 });
