a01087388e2ece8de7666acbbe1925d6c671e53c
rodriguezl <rodriguezl@ryanair.com>
2021-05-21 12:44:50 +0200

    red should_return_error_when_email_is_empty

---
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/src/TuduManayer.Domain/User/SignUp/SignUpUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace TuduManayer.Domain.User.SignUp
+{
+    public class SignUpUserService : ISignUpUserService
+    {
+        public ServiceExecutionResult SignUp(SignUpUserArgs args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ dotnet/react.mvc-core.postgres.dapper/TuduManager/test/TuduManayer.Domain.Test/User/SignUp/SignUpServiceShould.cs
@@ -0,0 +1,28 @@
+﻿using Shouldly;
+using TuduManayer.Domain.User.SignUp;
+using Xunit;
+
+namespace TuduManayer.Domain.Test.User.SignUp
+{
+    public class SignUpServiceShould
+    {
+        private ISignUpUserService service;
+
+        public SignUpServiceShould()
+        {
+            service = new SignUpUserService();
+        }
+
+        [Fact]
+        public void should_return_error_when_email_is_empty()
+        {
+            var args = new SignUpUserArgs(string.Empty, "password");
+            
+            var result = service.SignUp(args);
+            
+            result.IsOk.ShouldBeFalse();
+            result.Errors[0].FieldId.ShouldBe(nameof(SignUpUserArgs.Email));
+            result.Errors[0].ErrorCode.ShouldBe(ErrorCodes.Required);
+        }
+    }
+}
